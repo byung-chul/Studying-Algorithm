c031e5c3d2c9b8fd0314723180a73a192c95556f
윤병철 <30611446+byung-chul@users.noreply.github.com>
2024-07-22 21:30:52 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.44 ms, Memory: 66.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        int[] char_count = new int[26];
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            char_count[c - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if(char_count[i] != 1) {
+                continue;
+            }
+            sb.append(Character.toString((char) ('a' + i)));
+        }
+        String[] sa = sb.toString().split("");
+        Arrays.sort(sa);
+        
+        sb.setLength(0);
+        for (String split_s : sa) {
+            sb.append(split_s);
+        }
+        return sb.toString();
+    }
+}
