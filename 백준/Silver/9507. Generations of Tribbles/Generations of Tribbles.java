1a5d23f8af6d87e4ae2995b9253586e4d2970a7e
윤병철 <30611446+byung-chul@users.noreply.github.com>
2024-09-29 18:47:05 +0900

    [Silver IV] Title: Generations of Tribbles, Time: 104 ms, Memory: 13964 KB -BaekjoonHub

---
+++ 백준/Silver/9507. Generations of Tribbles/Generations of Tribbles.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+class Main {
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+        long[] dp = new long[68];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i < 68; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3] + dp[i - 4];
+        }
+
+        for (int i = 0; i < testCase; i++) {
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(dp[n]);
+        }
+    }
+}
